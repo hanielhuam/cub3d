d1c606125781688576769ea3bd05927cf2b94984
Haniel Huam Macedo Ferreira <hanielhuam@hotmail.com>
2026-04-29 19:47:34 -0300

    WIP: decidido a forma de fazer a consturção dos assets

--- src/create/assign_assets.c
+++ src/create/assign_assets.c
@@ -6,19 +6,28 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 19:43:23 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/04/28 20:27:21 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/04/29 19:45:36 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-
-
 static int	attribute_assets(t_game *game, t_token *token)
 {
-	if (token->type == TK_NO)
-		game->assets->no_texture = token->line;
-	if (token->type == 
+	t_tok_type	type;
+	static int	(*assets_constructors[6])(t_game *, char *line) = {
+		no_texture_constructor,
+		so_texture_constructor,
+		ea_texture_constructor,
+		we_texture_constructor,
+		floor_texture_constructor,
+		ceiling_texture_constructor
+	};
+
+	if (token->type == TK_NL)
+		return (0);
+	type = token->type;
+	return (assets_constructors[type](game, token->line));
 }
 
 int	assign_assets(t_game *game, t_list *tokens)
@@ -27,7 +36,7 @@ int	assign_assets(t_game *game, t_list *tokens)
 
 	while (tokens && ((t_token *)tokens->content)->type != TK_MAP)
 	{
-		attribute_assets(game, (t_token *)tokens->content);
+		if (attribute_assets(game, (t_token *)tokens->content))
 		tokens = tokens->next;
 	}
 	count = 0;
