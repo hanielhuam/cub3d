c393ea905405e54e7d3c178bdfa98ab8490a142f
Haniel Huam Macedo Ferreira <hanielhuam@hotmail.com>
2026-04-30 20:11:16 -0300

    WIP: Ainda estou nos consturtores, mas agora eu vou para o construtor de cor e para isso eu terei que fazer uma validação do que é um arrey de cor de pois pegar os valores

--- src/constructor/texture_constructor.c
+++ src/constructor/texture_constructor.c
@@ -6,34 +6,67 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 21:17:29 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/04/29 21:32:44 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/04/30 20:06:50 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static char	*extract_texture(char *line, char *errmsg)
+{
+	line = line + 3;
+	while (*line && ft_isspace(*line))
+		line++;
+	if (!*line)
+	{
+		ft_dprintf(STDERR_FILENO, errmsg);	
+		return (1);
+	}
+	return (dup_str(line));
+}
+
 int	no_texture_constructor(t_game *game, char *line)
 {
-	(void)game;
-	(void)line;
+	char	*texture;
+
+	texture = extract_texture(line, \
+			"Error: there is no value for NO texture\n");
+	if (texture)
+		return (1);
+	game->assets->no_texture = texture;
 	return (0);
 }
 
 int	so_texture_constructor(t_game *game, char *line)
 {
-	(void)game;
-	(void)line;
+	char	*texture;
+
+	texture = extract_texture(line, \
+			"Error: there is no value for SO texture\n");
+	if (texture)
+		return (1);
+	game->assets->so_texture = texture;
 	return (0);
 }
 int	ea_texture_constructor(t_game *game, char *line)
 {
-	(void)game;
-	(void)line;
+	char	*texture;
+
+	texture = extract_texture(line, \
+			"Error: there is no value for EA texture\n");
+	if (texture)
+		return (1);
+	game->assets->ea_texture = texture;
 	return (0);
 }
 int	we_texture_constructor(t_game *game, char *line)
 {
-	(void)game;
-	(void)line;
+	char	*texture;
+
+	texture = extract_texture(line, \
+			"Error: there is no value for WE texture\n");
+	if (texture)
+		return (1);
+	game->assets->we_texture = texture;
 	return (0);
 }
